0e675c305dfa6a369035206a2b7a155ca46eeb73
Chetan Mishra <chetan.s115@gmail.com>
2018-10-06 23:50:12 -0400

    cleaned up code

--- run.py
+++ run.py
@@ -4,107 +4,118 @@ from scipy import sparse
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn import decomposition, linear_model, metrics
+from sklearn import decomposition, linear_model, metrics, pipeline, base
 
 
-def create_connection():
-    mongo_url = "mongodb://localhost:27017"
-    client = MongoClient(mongo_url)
-    return client
+class DiscussionDataset:
+    db_name = "stack"
+    topics = set()
+
+    def __init__(self, mongo_url, data_folder):
+        self.client = MongoClient(mongo_url)
+
+        db = self.client[self.db_name]
+        json_files = [f for f in os.listdir(data_folder) if f[-5:] == ".json"]
+        for f in json_files:
+            topic_name = f[:-11]
+            topic = db[topic_name]
+            self.topics.add(topic_name)
+            with open(data_folder + "/" + f, "r") as fin:
+                for d in json.load(fin):
+                    doc = [d["title"], d["body"]]
+                    doc.extend([p["body"] for p in d["answers"]])
+                    topic.insert_one({"contents": " ".join(doc)})
+
+        corpus = {}
+        labels = []
+        for t in self.topics:
+            corpus[t] = [
+                d["contents"] for d in self.client[self.db_name][t].find({})
+            ]
+        flattened_docs = []
+        labels = []
+        for label, docs in corpus.items():
+            flattened_docs.extend(docs)
+            labels.extend([label for _ in range(len(docs))])
+        self.data = np.array(flattened_docs)
+        self.target = np.array(labels)
 
 
-def insert_data(client, db_name, data_folder):
-    db = client[db_name]
-    json_files = [f for f in os.listdir(data_folder) if f[-5:] == ".json"]
-    for f in json_files:
-        topic = db[f[:-11]]
-        with open(data_folder + "/" + f, "r") as fin:
-            for d in json.load(fin):
-                doc = []
-                doc.append(d["title"])
-                doc.append(d["body"])
-                doc.extend([p["body"] for p in d["answers"]])
-                topic.insert_one({"contents": " ".join(doc)})
-
-
-def tdm(client, db_name, topics):
-    corpus = {}
-    labels = []
-    for t in topics:
-        corpus[t] = [d["contents"] for d in client[db_name][t].find({})]
-
+def plurality_stemming(doc):
     analyzer = CountVectorizer(stop_words=None).build_analyzer()
-
-    def plurality_stemming(doc):
-        return [w[:-1] if w[-1] == "s" else w for w in analyzer(doc)]
-
-    transformer = CountVectorizer(
-        stop_words=None, min_df=10, analyzer=plurality_stemming)
-    flattened_docs = []
-    labels = []
-    for label, docs in corpus.items():
-        flattened_docs.extend(docs)
-        labels.extend([label for _ in range(len(docs))])
-    labels = np.array(labels)
-    train_feats, test_feats, train_labels, test_labels = train_test_split(
-        flattened_docs,
-        labels,
-        stratify=labels,
-        test_size=0.3,
-        random_state=40)
-    train_feats = transformer.fit_transform(train_feats)
-    test_feats = transformer.transform(test_feats)
-    return [
-        train_feats.toarray(),
-        test_feats.toarray(), train_labels, test_labels
-    ]
+    return [w[:-1] if w[-1] == "s" else w for w in analyzer(doc)]
 
 
-def reduce_dims(train_feats, test_feats):
-    n_components = 800
-    decomposer = decomposition.PCA(
-        n_components=n_components,
-        whiten=True,
-        svd_solver="randomized",
-        random_state=41)
-    train_feats = decomposer.fit_transform(train_feats)
-    print("{} components contain {} amount of variance".format(
-        n_components, decomposer.explained_variance_ratio_.sum()))
-    test_feats = decomposer.transform(test_feats)
-    return train_feats, test_feats
+class DenseTransformer(base.TransformerMixin):
+    def transform(self, X, y=None, **fit_params):
+        return X.todense()
+
+    def fit_transform(self, X, y=None, **fit_params):
+        self.fit(X, y, **fit_params)
+        return self.transform(X)
+
+    def fit(self, X, y=None, **fit_params):
+        return self
 
 
-def fit_model(train_feats, train_labels):
-    model = linear_model.LogisticRegressionCV(
-        n_jobs=4, random_state=40, solver="lbfgs", multi_class="ovr", cv=10)
-    model.fit(train_feats, train_labels)
-    return model
+class PipelineWithShapes(pipeline.Pipeline):
+    shapes = []
+
+    def predict(self, X, **predict_params):
+        Xt = X
+        for name, transform in self.steps[:-1]:
+            if transform is not None:
+                Xt = transform.transform(Xt)
+                self.shapes.append((name, Xt.shape))
+        return self.steps[-1][-1].predict(Xt, **predict_params)
 
 
 if __name__ == "__main__":
-    client = create_connection()
     data_folder = "/workingdir/StackExchange_posts"
-    db_name = "stack"
-    insert_data(client, db_name, data_folder)
-    print("data uploaded to mongodb\n")
+    mongo_url = "mongodb://localhost:27017"
+    dataset = DiscussionDataset(mongo_url, data_folder)
+    print("Data uploaded to mongodb\n")
 
-    topics = ["beer", "aviation", "pets", "astronomy", "outdoors"]
-    datasets = tdm(client, db_name, topics)
-    print("term-document matrix constructed")
-    print("data set split with n={} for training and n={} for test\n".format(
-        datasets[0].shape[0], datasets[1].shape[0]))
+    steps = [("vectorizer",
+              CountVectorizer(
+                  stop_words=None, min_df=10, analyzer=plurality_stemming)),
+             ("todense", DenseTransformer()),
+             ("reduce_dims",
+              decomposition.PCA(
+                  n_components=800,
+                  whiten=True,
+                  svd_solver="randomized",
+                  random_state=40)),
+             ("classifier",
+              linear_model.LogisticRegressionCV(
+                  n_jobs=4,
+                  random_state=40,
+                  solver="lbfgs",
+                  multi_class="ovr",
+                  cv=10))]
 
-    old_feat_size = datasets[0].shape[1]
-    datasets[0], datasets[1] = reduce_dims(datasets[0], datasets[1])
-    print("dimensionality reduced from {} to {}\n".format(
-        old_feat_size, datasets[0].shape[1]))
+    process = PipelineWithShapes(steps)
 
-    model = fit_model(datasets[0], datasets[2])
-    print("model trained with train error {} test error {}\n".format(
-        model.score(datasets[0], datasets[2]),
-        model.score(datasets[1], datasets[3])))
+    train_feats, test_feats, train_labels, test_labels = train_test_split(
+        dataset.data,
+        dataset.target,
+        stratify=dataset.target,
+        test_size=0.3,
+        random_state=40)
 
-    print("validation set confusion matrix:")
-    print(metrics.confusion_matrix(model.predict(datasets[1]), datasets[3]))
+    process.fit(train_feats, train_labels)
 
-    client.drop_database(db_name)
+    print("Validation set confusion matrix:")
+    print(
+        metrics.confusion_matrix(
+            process.predict(dataset.data), dataset.target))
+    print("Term-document matrix constructed with shape {}\n".format(
+        process.shapes[1][1]))
+    print("PCA used to reduce dimensionality to {} retaining {} variance\n".
+          format(process.shapes[2][1],
+                 process.steps[2][1].explained_variance_ratio_.sum()))
+    print("Model trained with train error {} test error {}\n".format(
+        process.score(train_feats, train_labels),
+        process.score(test_feats, test_labels)))
+
+    dataset.client.drop_database(dataset.db_name)
